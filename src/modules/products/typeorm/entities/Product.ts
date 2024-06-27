4ea5bb1eadec74a60d9396e08e8918afedb3f374
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-06-27 16:53:31 -0300

    :sparkles: feat: add entity `Product`

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @Column('timestamp with time zone')
+  created_at: Date;
+
+  @Column('timestamp with time zone')
+  updated_at: Date;
+}
