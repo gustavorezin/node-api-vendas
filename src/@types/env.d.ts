142f07cd00e6ee170600773ed49c54faf6d5bb7d
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-07-04 14:15:12 -0300

    :sparkles: feat: add `redis` e feito `cacheConfig`

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -3,5 +3,8 @@ declare namespace NodeJS {
     APP_SECRET: string;
     APP_API_URL: string;
     APP_WEB_URL: string;
+    REDIS_HOST: string;
+    REDIS_PORT: number;
+    REDIS_PASS: string;
   }
 }
