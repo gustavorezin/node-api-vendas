41dbdfed00d23513320420d8e1b1510a0ae385d3
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-07-04 13:53:20 -0300

    :sparkles: feat: add lib `classTransformer`

--- src/modules/users/controllers/ProfileController.ts
+++ src/modules/users/controllers/ProfileController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express';
 import { ShowProfileService } from '../services/ShowProfileService';
 import { UpdateProfileService } from '../services/UpdateProfileService';
+import { instanceToInstance } from 'class-transformer';
 
 export class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
     const showProfile = new ShowProfileService();
     const userId = request.user.id;
     const user = await showProfile.execute({ userId });
-    return response.json(user);
+    return response.json(instanceToInstance(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -21,6 +22,6 @@ export class ProfileController {
       password,
       oldPassword
     });
-    return response.json(user);
+    return response.json(instanceToInstance(user));
   }
 }
--- src/modules/users/controllers/SessionsController.ts
+++ src/modules/users/controllers/SessionsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { CreateSessionService } from '../services/CreateSessionService';
+import { instanceToInstance } from 'class-transformer';
 
 export class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -8,6 +9,6 @@ export class SessionsController {
     const createSession = new CreateSessionService();
     const session = await createSession.execute({ email, password });
 
-    return response.json(session);
+    return response.json(instanceToInstance(session));
   }
 }
