31162d8265a7acb55cf625d072bdbab6689399b1
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-07-05 14:55:39 -0300

    :sparkles: feat: add `DiskStorageProvider` salvar local

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,11 +3,13 @@ import path from 'path';
 import crypto from 'crypto';
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export const uploadConfig = {
   diretory: uploadFolder,
+  tempFolder,
   storage: multer.diskStorage({
-    destination: uploadFolder,
+    destination: tempFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
---
+++ src/shared/providers/StorageProvider/DiskStorageProvider.ts
@@ -0,0 +1,24 @@
+import { uploadConfig } from '@config/upload';
+import fs from 'fs';
+import path from 'path';
+
+export class DiskStorageProvider {
+  public async saveFile(file: string) {
+    await fs.promises.rename(
+      path.resolve(uploadConfig.tempFolder, file),
+      path.resolve(uploadConfig.diretory, file)
+    );
+  }
+
+  public async deleteFile(file: string) {
+    const filePath = path.resolve(uploadConfig.diretory, file);
+
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return;
+    }
+
+    await fs.promises.unlink(filePath);
+  }
+}
