12848e65a316167c9605df94e0aaffa517922582
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-07-14 20:19:06 -0300

    :sparkles: feat: add validação e-mail `CreateCustomerService`

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 import { ICreateCustomer } from '../domain/models/ICreateCustomer';
 import { ICustomersRepository } from '../domain/repositories/ICustomersRepository';
+import { AppError } from '@shared/errors/AppError';
 
 @injectable()
 export class CreateCustomerService {
@@ -10,6 +11,12 @@ export class CreateCustomerService {
   ) {}
 
   public async execute({ name, email }: ICreateCustomer) {
+    const emailExists = await this.customersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Email address already used.');
+    }
+
     const customer = await this.customersRepository.generate({
       name,
       email
